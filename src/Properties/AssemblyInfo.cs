27a555b2aee046eacd9b458b23540bc405ae4371
Max Koryukov <maxkoryukov@yandex.ru>
2015-07-07 19:16:33 +0500

    * fix name... huge commit, yeah..

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Max Koryukov")]
+[assembly: AssemblyCopyright("Maksim Koryukov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
