ad9fed3dfe000fbc9eda0a85ad5886e905937680
Max Koryukov <maxkoryukov@yandex.ru>
2015-07-21 02:49:10 +0500

    + Pair

---
+++ src/Collections/Generic/Pair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MK.Collections.Generic
+{
+	public class Pair<M, N>
+	{
+		public M A { get; set; }
+		public N B { get; set; }
+	}
+
+	public class Pair<M>
+		:Pair<M, M>
+	{
+		public void Swap()
+		{
+			M t;
+			t = this.A;
+			this.A = this.B;
+			this.B = t;
+		}
+	}
+}
