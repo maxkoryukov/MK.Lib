937540f74f3050e3165b05eadb2de916be7a0fe3
Max Koryukov <maxkoryukov@yandex.ru>
2015-07-21 16:28:30 +0500

    * Pairs

--- src/Collections/Generic/Pair.cs
+++ src/Collections/Generic/Pair.cs
@@ -2,7 +2,19 @@
 
 namespace MK.Collections.Generic
 {
+	public interface IPair<M, N>
+	{
+		M A { get; set; }
+		N B { get; set; }
+	}
+
+	public interface IPair<M>
+		: IPair<M, M>
+	{
+	}
+
 	public class Pair<M, N>
+		: IPair<M, N>
 	{
 		public M A { get; set; }
 		public N B { get; set; }
@@ -10,6 +22,8 @@ namespace MK.Collections.Generic
 
 	public class Pair<M>
 		:Pair<M, M>
+		,IPair<M, M>
+		,IPair<M>	
 	{
 		public void Swap()
 		{
