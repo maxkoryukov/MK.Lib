8b61a1af9a98651acb8344a4fc1587d638e07f98
Max Koryukov <maxkoryukov@yandex.ru>
2015-07-30 01:19:31 +0500

    * NullableComparer :: CompareTo * DateTime :: IsInRange

---
+++ src/NullableCompareExt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MK
+{
+	public static class NullableCompareExt
+	{
+		public static int Compare<T>(this Nullable<T> a, Nullable<T> b)
+			where T : struct, IComparable<T>
+		{
+			if (!a.HasValue && !b.HasValue)
+				return 0;
+			if (a.HasValue && b.HasValue)
+				return a.Value.CompareTo(b.Value);
+			if (a.HasValue)
+				return -1;
+
+			//if (b.HasValue)
+			return 1;
+		}
+	}
+}
