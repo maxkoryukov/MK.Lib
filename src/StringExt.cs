cf6f36a2740ed33f8a33a8377addd1d397c9be09
Max Koryukov <maxkoryukov@yandex.ru>
2015-08-30 09:01:16 +0500

    * pwd generator

--- src/StringExt.cs
+++ src/StringExt.cs
@@ -13,5 +13,32 @@ namespace MK
 				s = null;
 			return s;
 		}
+
+		public const string RandomPattern = "1234567890abcdefghijklmnopqrstuvwxyz";
+
+		public static string AppendRandom(this string src, int length, string char_domain = null)
+		{
+			int src_len = (string.IsNullOrEmpty(src)) ? 0 : src.Length;
+
+			if (src_len >= length)
+				return src;
+
+			if (string.IsNullOrEmpty(char_domain))
+				char_domain = RandomPattern;
+			
+			int len = char_domain.Length;
+			var r = new System.Random();
+
+			if (null == src)
+				src = "";
+			
+			while (src_len < length)
+			{
+				src += char_domain[r.Next(len)];
+				src_len++;
+			}
+
+			return src;
+		}
 	}
 }
