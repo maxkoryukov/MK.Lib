b5ad811df5ae2ed7ebd1f069858bc799736deb30
Max Koryukov <maxkoryukov@yandex.ru>
2015-09-06 06:33:28 +0500

    Remove the diacritics extension for string

--- src/StringExt.cs
+++ src/StringExt.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Globalization;
 
 namespace MK
 {
@@ -39,6 +41,30 @@ namespace MK
 			}
 
 			return src;
+		}
+
+		/// <summary>
+		/// Removes the diacritics.
+		/// </summary>
+		/// <seealso cref="http://www.siao2.com/2007/05/14/2629747.aspx"/>
+		/// <seealso cref="http://stackoverflow.com/questions/249087"/>
+		/// <returns>The diacritics.</returns>
+		/// <param name="text">Text.</param>
+		public static string RemoveDiacritics(this string text) 
+		{
+			var ns = text.Normalize(NormalizationForm.FormD);
+			var sb = new StringBuilder();
+
+			foreach (var c in ns)
+			{
+				var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+				if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+				{
+					sb.Append(c);
+				}
+			}
+
+			return sb.ToString().Normalize(NormalizationForm.FormC);
 		}
 	}
 }
