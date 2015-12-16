101f60a88487eadd3af79afb6969a7476d619f08
maxkoryukov <maxkoryukov@yandex.ru>
2015-12-17 02:15:27 +0500

    * Tests fixed - uppercase for chars * nunit 2.3 returns!!!!

--- src/MK.WebLib.T/Web/FormDataCollectionTest.cs
+++ src/MK.WebLib.T/Web/FormDataCollectionTest.cs
@@ -23,7 +23,7 @@ namespace MK.Web
 			var y = new FormDataCollection();
 			y.Add(key1, val1);
 
-			Assert.AreEqual("form.name%5bvalue.1%5d=Igor+Nikolaev+12+%26+Natasha+X", y.ToString());
+			Assert.AreEqual("form.name%5Bvalue.1%5D=Igor+Nikolaev+12+%26+Natasha+X", y.ToString());
 		}
 
 		[Test]
@@ -34,7 +34,7 @@ namespace MK.Web
 			y.Add("form.name[value.1]", "Igor Nikolaev 12 & Natasha X");
 			y.Add("simple-string-x127", "12/3*x\\67ye.?!@#$%^&*()");
 
-			Assert.AreEqual("form.name%5bvalue.1%5d=Igor+Nikolaev+12+%26+Natasha+X&simple-string-x127=12%2f3*x%5c67ye.%3f!%40%23%24%25%5e%26*()", y.ToString());
+			Assert.AreEqual("form.name%5Bvalue.1%5D=Igor+Nikolaev+12+%26+Natasha+X&simple-string-x127=12%2F3*x%5C67ye.%3F!%40%23%24%25%5E%26*()", y.ToString());
 		}
 	
 		[Test]
@@ -45,14 +45,14 @@ namespace MK.Web
 			y.Add("form.name[value.1]", "Igor Nikolaev 12 & Natasha X");
 			y.Add("alphavit", "abcdefghijklmnopqrstuvwxuz");
 
-			Assert.AreEqual("form.name%5bvalue.1%5d=Igor+Nikolaev+12+%26+Natasha+X&alphavit=abcdefghijklmnopqrstuvwxuz", y.ToString());
+			Assert.AreEqual("form.name%5Bvalue.1%5D=Igor+Nikolaev+12+%26+Natasha+X&alphavit=abcdefghijklmnopqrstuvwxuz", y.ToString());
 
 			y = new FormDataCollection();
 
 			y.Add("form.name[value.1]", "Igor Nikolaev 12 & Natasha X");
 			y.Add("alphavit", "ABCDEFGHIJKLMNOPQRSTUVWXUZ");
 
-			Assert.AreEqual("form.name%5bvalue.1%5d=Igor+Nikolaev+12+%26+Natasha+X&alphavit=ABCDEFGHIJKLMNOPQRSTUVWXUZ", y.ToString());
+			Assert.AreEqual("form.name%5Bvalue.1%5D=Igor+Nikolaev+12+%26+Natasha+X&alphavit=ABCDEFGHIJKLMNOPQRSTUVWXUZ", y.ToString());
 		}
 
 		[Test]
@@ -63,7 +63,7 @@ namespace MK.Web
 			y.Add("form.name[value.1]", "test");
 			y.Add("alphavit", "MOISÉS 6140-3801");
 
-			Assert.AreEqual("form.name%5bvalue.1%5d=test&alphavit=MOIS%C3%89S+6140-3801", y.ToString());
+			Assert.AreEqual("form.name%5Bvalue.1%5D=test&alphavit=MOIS%C3%89S+6140-3801", y.ToString());
 		}
 	}
 }
