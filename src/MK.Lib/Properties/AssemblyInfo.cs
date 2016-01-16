f64af25d55ef64e33a90cd09b1d20d1b694cc813
maxkoryukov <maxkoryukov@yandex.ru>
2016-01-16 11:14:50 +0500

    Small fixes for assembly info

--- src/MK.Lib/Properties/AssemblyInfo.cs
+++ src/MK.Lib/Properties/AssemblyInfo.cs
@@ -1,20 +1,28 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+[assembly: AssemblyTitle("MK.Lib")]
+[assembly: AssemblyDescription("My useful C# things")]
+[assembly: AssemblyProduct("MK.Lib")]
+[assembly: AssemblyCopyright("© 2015-2016 Max Koryukov")]
 
-[assembly: AssemblyTitle("MK.WebLib")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Maksim Koryukov")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 
-[assembly: InternalsVisibleTo("MK.WebLib.T")]
+[assembly: InternalsVisibleTo("MK.Lib.T")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+//[assembly: CLSCompliant (true)]
+//[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+//[assembly: CompilationRelaxations (8)]
+//[assembly: TargetFramework (".NETFramework,Version=v4.0", FrameworkDisplayName = ".NET Framework 4")]
 
+// autofill on CI
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
